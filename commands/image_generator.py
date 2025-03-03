b34c75dce648ee2ab295ba3d5a26f7cd994d4e15
xdaniel-lim <roll4danmage@gmail.com>
2025-03-03 12:33:54 +0800

    dynamic font sizing, datetime, and byline

--- commands/image_generator.py
+++ commands/image_generator.py
@@ -1,25 +1,52 @@
 from PIL import Image, ImageDraw, ImageFont
+from datetime import datetime
 
 def create_quote_image(path, text: str, author: str = "wuping"):
     image = Image.new("RGBA", (512, 512), (0, 0, 0, 0))
     draw = ImageDraw.Draw(image)
 
-    font = ImageFont.truetype("./DINCondensed-Regular.ttf", 85)
-
+    font_size = 85
     quote_array = f'"{text}"'.split(" ")
-    current_text = ""
+    current_year = datetime.now().year
+    yr = str(current_year)
+    if yr[-3] == '0':
+        yr = yr[:-3] + 'k' + yr[-2:]
 
-    for word in quote_array:
-        left, top, right, bottom = draw.textbbox((20, 400), current_text + word + " ", stroke_width=12, font=font)
-        if right > 512:
-            current_text += "\n"
-        current_text += word + " "
+    while font_size > 10:
 
-    current_text += f"\n    -{author} 2k24"
+        stroke = int(font_size*0.142)
+        font = ImageFont.truetype("./DINCondensed-Regular.ttf", font_size)
+        current_text = ""
 
-    _, _, _, height = draw.textbbox((12, -5), current_text, stroke_width=12, font=font)
+        for word in quote_array:
+            _, _, right, _ = draw.textbbox((20, 400), current_text + word + " ", stroke_width=stroke, font=font)
+            if right > 512:
+                current_text += "\n"
+            current_text += word + " "
 
-    draw.text((20, (512 - height) / 2), current_text, fill=(37, 33, 58, 255), stroke_fill=(255, 255, 255, 255), stroke_width=12, font=font)
+        ### BYLINE
+        byline = f"-{author}, {yr}"
+        max_line = len(byline)
+        right = 0
+        _, _, right, _ = draw.textbbox((20, 400), byline, stroke_width=stroke, font=font)
+        if right > 512-stroke:
+            font_size = font_size//1.1
+            continue
+        while right < 512-stroke:
+            max_line += 1
+            _, _, right, _ = draw.textbbox((20, 400), byline.rjust(max_line), stroke_width=stroke, font=font)
+        current_text += '\n' + byline.rjust(max_line)
+        
+        ### CHECK BOUNDS
+
+        _, top, _, bottom = draw.textbbox((20, 400), current_text, stroke_width=stroke, font=font)
+        if bottom - top > 512:
+            font_size = font_size//1.1
+        else:
+            break
+
+    _, _, _, height = draw.textbbox((12, -5), current_text, stroke_width=stroke, font=font)
+    draw.text((20, (512 - height) / 2), current_text, fill=(37, 33, 58, 255), stroke_fill=(255, 255, 255, 255), stroke_width=stroke, font=font)
 
     image.save(path)
 
